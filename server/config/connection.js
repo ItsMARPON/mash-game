b8cfe00520e21f85aa59935b2beae3277857f30c
RileyAlt <rileyaltenburg@gmail.com>
2023-05-16 12:51:02 -0500

    Connection need database

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernUsers'
+, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+
+
+module.exports = mongoose.connection;
