b1364547c6349f8390812e5fae382289428d9331
RileyAlt <rileyaltenburg@gmail.com>
2023-05-16 15:55:46 -0500

    user model fix

--- server/models/User.js
+++ server/models/User.js
@@ -1,25 +1,27 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const userSchema = new Schema({
-    username: {
-        type: String, 
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+/, 'Must match an email adress!'],
-    },
-    password: {
-        type: String, 
-        required: true,
-        minlength: 5,
+const userSchema = new Schema(
+    {
+        username: {
+            type: String, 
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+/, 'Must match an email adress!'],
+        },
+        password: {
+            type: String, 
+            required: true,
+            minlength: 5,
+        }
     }
-});
+);
 
 userSchema.pre('save', async function (next) {
     if (this.isNew || this.isModified('password')) {
@@ -33,6 +35,10 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.isCorrectPassword = async function (password) {
     return bcrypt.compare(password, this.password);
 };
+
+userSchema.virtual('storyCount').get(function () {
+    return this.savedStories.length;
+});
 
 const User = model('User', userSchema);
 
