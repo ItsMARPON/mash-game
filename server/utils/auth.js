f3f79c7039e1f7f670742bb9f3b0e1825ead313b
RileyAlt <rileyaltenburg@gmail.com>
2023-05-18 16:15:00 -0500

    removing comments

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,16 +1,12 @@
 const jwt = require("jsonwebtoken");
-
-// set token secret and expiration date
 const secret = "mysecretssshhhhhhh";
 const expiration = "2h";
 
 module.exports = {
-  // function for our authenticated routes
   authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.body or req.query or req.headers
+
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
@@ -18,14 +14,13 @@ module.exports = {
     if (!token) {
       return req;
     }
-    // verify token and get user data out of it
+
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log("Invalid token");
     }
-    // return the request object so it can be passed to the resolver as `context`
     return req;
   },
 
