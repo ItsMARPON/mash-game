fc950b3ed6800fd6301c1dfb40eb57e6477329e8
Mary <itsmaryyang@gmail.com>
2023-05-23 13:50:36 -0500

    Remove build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
