9e1960a0080d03418e6dbd4737f3fc8fa10409c9
Mary <itsmaryyang@gmail.com>
2023-05-23 19:27:34 -0500

    Update Signup for useMutation and ADD_USER

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,36 +1,30 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 
-import {useMutation} from '@apollo/client';
+import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 
-import Auth from '../utils/auth';
-
-
+import Auth from "../utils/auth";
 
 export interface SignupProps {
-  // userFormState: string;
-  // setUserFormState: React.Dispatch<React.SetStateAction<string>>;
   username: string;
   email: string;
   password: string;
 }
 
-const Signup = ({ username, email, password }: SignupProps) => {
+const Signup = ({ username, email, password }: SignupProps): JSX.Element => {
   const [userFormState, setUserFormState] = useState({
     username: "",
     email: "",
     password: "",
   });
 
-  const [addUser, {loading, error}] = useMutation(ADD_USER);
-  if(loading) return 'Submitting...';
-  if (error) return `Submission error! ${error.message}`;
+  const [addUser] = useMutation(ADD_USER);
+ 
 
   // Update the state based on user input changes to form
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-
 
     setUserFormState({
       ...userFormState,
@@ -48,30 +42,26 @@ const Signup = ({ username, email, password }: SignupProps) => {
       event.stopPropagation();
     }
     try {
-      const {data} = await addUser({variables: {...userFormState}});
+      const { data } = await addUser({ variables: { ...userFormState } });
 
-      Auth.login(data.addUser.token)
-
-    } catch (err){
+      Auth.login(data.addUser.token);
+    } catch (err) {
       console.log(err);
     }
 
     setUserFormState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
-
-
+      username: "",
+      email: "",
+      password: "",
+    });
   };
-
 
   return (
     <div className="w-full max-w-xs">
       <form
         className="signup-input bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={handleFormSubmit}>
+        onSubmit={handleFormSubmit}
+      >
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
@@ -89,36 +79,47 @@ const Signup = ({ username, email, password }: SignupProps) => {
           />
         </div>
         <div className="mb-6">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
             Email
           </label>
-        <input
-          className="form-input shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="Your email"
-          name="email"
-          type="email"
-          value={userFormState.email}
-          onChange={handleChange}
-        />
+          <input
+            className="form-input shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Your email"
+            name="email"
+            type="email"
+            value={userFormState.email}
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-6">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
             Password
           </label>
-        <input
-          className="form-input shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="*********"
-          name="password"
-          type="password"
-          value={userFormState.password}
-          onChange={handleChange}
-        />
-        <p className="text-red-500 text-xs italic">Please create a password.</p>
-      </div>
-      <div className="flex items-center justify-between">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-          Sign up
-        </button>
+          <input
+            className="form-input shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="*********"
+            name="password"
+            type="password"
+            value={userFormState.password}
+            onChange={handleChange}
+          />
+          <p className="text-red-500 text-xs italic">
+            Please create a password.
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign up
+          </button>
         </div>
       </form>
       <p className="text-center text-gray-500 text-xs">
