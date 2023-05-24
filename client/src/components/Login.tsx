0d26f3fa3c5a4e5cb33689c6d2081ea230c9f34c
Connor S <constroh@gmail.com>
2023-05-23 20:22:35 -0500

    How.tsx

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -6,7 +6,7 @@ const Login = (props: Props) => {
   return (
     <section className="w-screen">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md bg-gray-500">
+        <div className="w-full rounded-lg shadow-2xl shadow-black dark:border md:mt-0 sm:max-w-md bg-gray-500">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
               Log in to your account
